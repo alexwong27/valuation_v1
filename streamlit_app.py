db8abf28b1c6a4de55ae5a410a4365858d40e43b
alexwong27 <41120315+alexwong27@users.noreply.github.com>
2023-03-24 02:18:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,24 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(page_title="Test Decision Tree", page_icon="🌲", layout="centered")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title("🖥 Valuation tool")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+form = st.form(key="annotation")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+with st.form("template_form"):
+    left, right = st.columns((1, 10))
+    M = right.input("Margin", value=0.3)
+    left, right = st.columns(2)
+    G = left.input("Growth", value=0.15)
+    R = right.input("Retention", value=0.95)
+    N = left.input("Average MRR", value=25000)
+    
+    Valuation = A * N * 12
+    submit = st.form_submit_button()
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if submitted:
+   
+    st.success(Valuation)
+    st.balloons()
