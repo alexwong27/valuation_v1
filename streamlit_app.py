c6021499b0258eb1550676832416f7ae290bb24c
alexwong27 <41120315+alexwong27@users.noreply.github.com>
2023-04-03 10:09:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,67 +19,67 @@ with st.form("my_form"):
     submit = st.form_submit_button()
     
     
-if M < 0.3 and G < 0.15 and R < 0.9:
+if M < 0.3 and G < 0.1 and R < 0.9:
     A = 1.6
-elif M < 0.3 and G < 0.15 and R < 0.95:
+elif M < 0.3 and G < 0.1 and R < 0.95:
     A = 1.7
-elif M < 0.3 and G < 0.15 and R >= 0.95:
+elif M < 0.3 and G < 0.1 and R >= 0.95:
     A = 1.8
 
-elif M < 0.3 and G < 0.5 and R < 0.9:
+elif M < 0.3 and G < 0.35 and R < 0.9:
     A = 1.9
-elif M < 0.3 and G < 0.5 and R < 0.95:
+elif M < 0.3 and G < 0.35 and R < 0.95:
     A = 2.0
-elif M < 0.3 and G < 0.5 and R >= 0.95:
+elif M < 0.3 and G < 0.35 and R >= 0.95:
     A = 2.1
 
-elif M < 0.3 and G >= 0.5 and R < 0.9:
+elif M < 0.3 and G >= 0.35 and R < 0.9:
     A = 2.2
-elif M < 0.3 and G >= 0.5 and R < 0.95:
+elif M < 0.3 and G >= 0.35 and R < 0.95:
     A = 2.3
-elif M < 0.3 and G >= 0.5 and R >= 0.95:
+elif M < 0.3 and G >= 0.35 and R >= 0.95:
     A = 2.4
 
-elif M < 0.6 and G < 0.15 and R < 0.9:
+elif M < 0.6 and G < 0.1 and R < 0.9:
     A = 2.0
-elif M < 0.6 and G < 0.15 and R < 0.95:
+elif M < 0.6 and G < 0.1 and R < 0.95:
     A = 2.21
-elif M < 0.6 and G < 0.15 and R >= 0.95:
+elif M < 0.6 and G < 0.1 and R >= 0.95:
     A = 2.42
 
-elif M < 0.6 and G < 0.5 and R < 0.9:
+elif M < 0.6 and G < 0.35 and R < 0.9:
     A = 2.63
-elif M < 0.6 and G < 0.5 and R < 0.95:
+elif M < 0.6 and G < 0.35 and R < 0.95:
     A = 2.84
-elif M < 0.6 and G < 0.5 and R >= 0.95:
+elif M < 0.6 and G < 0.35 and R >= 0.95:
     A = 3.05
     
-elif M < 0.6 and G >= 0.5 and R < 0.9:
+elif M < 0.6 and G >= 0.35 and R < 0.9:
     A = 3.26
-elif M < 0.6 and G >= 0.5 and R < 0.95:
+elif M < 0.6 and G >= 0.35 and R < 0.95:
     A = 3.47
-elif M < 0.6 and G >= 0.5 and R >= 0.95:
+elif M < 0.6 and G >= 0.35 and R >= 0.95:
     A = 3.68
 
-elif M >= 0.6 and G < 0.15 and R < 0.9:
+elif M >= 0.6 and G < 0.1 and R < 0.9:
     A = 2.84
-elif M >= 0.6 and G < 0.15 and R < 0.95:
+elif M >= 0.6 and G < 0.1 and R < 0.95:
     A = 3.05
-elif M >= 0.6 and G < 0.15 and R >= 0.95:
+elif M >= 0.6 and G < 0.1 and R >= 0.95:
     A = 3.25
 
-elif M >= 0.6 and G < 0.5 and R < 0.9:
+elif M >= 0.6 and G < 0.35 and R < 0.9:
     A = 3.45
-elif M >= 0.6 and G < 0.5 and R < 0.95:
+elif M >= 0.6 and G < 0.35 and R < 0.95:
     A = 3.75
-elif M >= 0.6 and G < 0.5 and R >= 0.95:
+elif M >= 0.6 and G < 0.35 and R >= 0.95:
     A = 4.1
 
-elif M >= 0.6 and G >= 0.5 and R < 0.9:
+elif M >= 0.6 and G >= 0.35 and R < 0.9:
     A = 3.05
-elif M >= 0.6 and G >= 0.5 and R < 0.95:
+elif M >= 0.6 and G >= 0.35 and R < 0.95:
     A = 4.55
-elif M >= 0.6 and G >= 0.5 and R >= 0.95:
+elif M >= 0.6 and G >= 0.35 and R >= 0.95:
     A = 4.85
 
 
